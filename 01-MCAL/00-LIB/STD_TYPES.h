6c287406323a257408f47bf65ab1d3661abed405
Marev-Wasim <marevwasim79@gmail.com>
2026-09-22 18:40:06 +0300

    STD_TYPES.h edit

--- 01-MCAL/00-LIB/STD_TYPES.h
+++ 01-MCAL/00-LIB/STD_TYPES.h
@@ -1,20 +1,17 @@
-/***************************************************************************************/
-/****************************  IMT School Training Center ******************************/
-/***************************************************************************************/
 #ifndef STD_TYPES_H
 #define STD_TYPES_H
 
-typedef unsigned char u8;
-typedef unsigned short int u16;
-typedef unsigned long  int u32;
+typedef unsigned char       u8;
+typedef unsigned short      u16;
+typedef unsigned long       u32;
+typedef signed char         s8;
+typedef signed short        s16;
+typedef signed long         s32;
+typedef float               f32;
+typedef double              f64;
 
-typedef signed char s8;
-typedef signed short int s16;
-typedef signed long  int s32;
-
-typedef float  f32;
-typedef double f64;
-
-#define NULL (void *)0
+#ifndef NULL
+#define NULL    ((void*)0)
+#endif
 
 #endif
