1d88dbc4bc7ab12ebb523eec7ad72bb796f2e59a
Marev-Wasim <marevwasim79@gmail.com>
2026-09-22 22:02:54 +0300

    ADC_program.c edit

--- 01-MCAL/02-ADC/ADC_program.c
+++ 01-MCAL/02-ADC/ADC_program.c
@@ -8,22 +8,26 @@ void ADC_voidInit(void)
 {
 	/* Configure Voltage Reference */
 	#if ADC_REFVOLT == ADC_AVCC
-		ADC_u8_ADMUX_REG = (1 << ADC_u8_ADMUX_REFS0);
+		ADC_u8_ADMUX_REG = 0;
+		SET_BIT(ADC_u8_ADMUX_REG, ADC_u8_ADMUX_REFS0);
 	#elif ADC_REFVOLT == ADC_INTERNAL
-		ADC_u8_ADMUX_REG = (1 << ADC_u8_ADMUX_REFS1) | (1 << ADC_u8_ADMUX_REFS0);
+		ADC_u8_ADMUX_REG = 0;
+		SET_BIT(ADC_u8_ADMUX_REG, ADC_u8_ADMUX_REFS1);
+		SET_BIT(ADC_u8_ADMUX_REG, ADC_u8_ADMUX_REFS0);
 	#elif ADC_REFVOLT == ADC_AREF
 		ADC_u8_ADMUX_REG = 0;
 	#endif
 
 	/* Configure Left / Right Adjustment */
 	#if ADC_ADJUST_CTRL == LEFT
-		ADC_u8_ADMUX_REG |= (1 << ADC_u8_ADMUX_ADLAR);
+		SET_BIT(ADC_u8_ADMUX_REG, ADC_u8_ADMUX_ADLAR);
 	#elif ADC_ADJUST_CTRL == RIGHT
-		ADC_u8_ADMUX_REG &= ~(1 << ADC_u8_ADMUX_ADLAR);
+		CLR_BIT(ADC_u8_ADMUX_REG, ADC_u8_ADMUX_ADLAR);
 	#endif
 
 	/* Configure Prescaler & Enable ADC */
-	ADC_u8_ADCSRA_REG = (1 << ADC_u8_ADCSRA_ADEN) | (ADC_PRESCALAR_VAL & 0x07);
+	ADC_u8_ADCSRA_REG = (ADC_PRESCALAR_VAL & 0x07);
+	SET_BIT(ADC_u8_ADCSRA_REG, ADC_u8_ADCSRA_ADEN);
 }
 
 u16 ADC_u16ReadSynchronus(u8 Copy_u8Channel)
@@ -32,10 +36,10 @@ u16 ADC_u16ReadSynchronus(u8 Copy_u8Channel)
 	ADC_u8_ADMUX_REG = (ADC_u8_ADMUX_REG & ADC_CHANNEL_MASK) | (Copy_u8Channel & 0x07);
 
 	/* Start conversion */
-	ADC_u8_ADCSRA_REG |= (1 << ADC_u8_ADCSRA_ADSC);
+	SET_BIT(ADC_u8_ADCSRA_REG, ADC_u8_ADCSRA_ADSC);
 
 	/* Wait for conversion to complete */
-	while (ADC_u8_ADCSRA_REG & (1 << ADC_u8_ADCSRA_ADSC));
+	while (GET_BIT(ADC_u8_ADCSRA_REG, ADC_u8_ADCSRA_ADSC));
 
 	/* Return result based on alignment configuration */
 	#if ADC_ADJUST_CTRL == RIGHT
