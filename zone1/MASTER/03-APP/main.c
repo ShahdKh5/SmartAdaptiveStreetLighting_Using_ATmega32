974696986d24b56706b41cda2e86778134bc632d
OmniaMansour677 <Omniamansour101@gmail.com>
2026-09-23 23:34:44 +0300

    Implement system initialization and main loop functionality

--- zone1/MASTER/03-APP/main.c
+++ zone1/MASTER/03-APP/main.c
@@ -6,17 +6,16 @@
 #include "../01-MCAL/00-LIB/STD_TYPES.h"
 #include "APP_interface.h"
 
-int main(void) {
-    /* Initialize the system */
-    /* APP_voidSystemInit(); */
-    
-    while(1) {
-        /* TODO: 
-           1. Call APP_voidControlLighting() to manage the adaptive illumination.
-           2. Call APP_voidUpdateDisplay() to refresh the LCD.
-           3. Add a small delay for stability.
-        */
+int main(void)
+{
+    APP_voidSystemInit();
+
+    while (1)
+    {
+        APP_voidControlLighting();
+        APP_voidUpdateDisplay();
+        _delay_ms(100);
     }
-    
+
     return 0;
 }
