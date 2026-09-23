fded4be231e0138883566f4cfaff71794d21ecf0
Shahd Khalil <114525646+ShahdKh5@users.noreply.github.com>
2026-09-23 22:52:55 +0300

    Update LCD_interface.h

--- zone1/MASTER/02-HAL/01-LCD/LCD_interface.h
+++ zone1/MASTER/02-HAL/01-LCD/LCD_interface.h
@@ -6,7 +6,7 @@
 void LCD_voidInit(void);
 void LCD_voidSendInstruction(u8 Copy_u8instruction);
 void LCD_voidWriteChar(u8 Copy_u8Data);
-void LCD_voidWriteString(u8 *Copy_u8String, u8 Copy_u8Size);
+void LCD_voidWriteString(u8 *Copy_u8String);
 void LCD_voidGotoXY(u8 Copy_u8Row, u8 Copy_u8Column);
 void LCD_voidClearDisplay(void);
 void LCD_voidWriteSpecialChar(u8 *Copy_u8Pattern, u8 Copy_u8PatternNumber ,u8 Copy_u8Row, u8 Copy_u8Column);
