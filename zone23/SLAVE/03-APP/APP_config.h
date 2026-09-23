4b3e23be5cb5e06b63a15d8f7bc41681af03e692
OmniaMansour677 <Omniamansour101@gmail.com>
2026-09-24 02:41:11 +0300

    Remove unnecessary comments from APP_config.h
    Removed comments about flashing slave code for different boards.

--- zone23/SLAVE/03-APP/APP_config.h
+++ zone23/SLAVE/03-APP/APP_config.h
@@ -1,11 +1,6 @@
 #ifndef APP_CONFIG_H
 #define APP_CONFIG_H
 
-/* Flash this SAME slave code onto both remaining boards.
-   The ONLY thing that changes between them is this line:
-     - Board driving Zone2 -> SLAVE_u8_MY_ZONE = LINK_u8_ZONE2
-     - Board driving Zone3 -> SLAVE_u8_MY_ZONE = LINK_u8_ZONE3
-   (LINK_u8_ZONE2 / LINK_u8_ZONE3 are defined in LINK_protocol.h) */
 #include "LINK_protocol.h"
 
 #define SLAVE_u8_MY_ZONE   LINK_u8_ZONE2
