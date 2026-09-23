9274bc8fd65f5d7c640f297c7d0e830582ea7a17
Shahd Khalil <114525646+ShahdKh5@users.noreply.github.com>
2026-09-23 22:30:17 +0300

    Remove LCD_voidSendInstruction declaration
    Removed the function declaration for LCD_voidSendInstruction.

--- zone1/MASTER/02-HAL/01-LCD/LCD_interface.h
+++ zone1/MASTER/02-HAL/01-LCD/LCD_interface.h
@@ -4,7 +4,6 @@
 #include "../../01-MCAL/00-LIB/STD_TYPES.h"
 
 void LCD_voidInit(void);
-void LCD_voidSendInstruction(u8 Copy_u8instruction);
 void LCD_voidWriteChar(u8 Copy_u8Data);
 void LCD_voidWriteString(u8 *Copy_u8String, u8 Copy_u8Size);
 void LCD_voidGotoXY(u8 Copy_u8Row, u8 Copy_u8Column);
