48149a1dd98d775639e8158e995159c4abfc2aa7
Shahd Khalil <114525646+ShahdKh5@users.noreply.github.com>
2026-09-23 22:27:14 +0300

    Update LCD_private.h

--- zone1/MASTER/02-HAL/01-LCD/LCD_private.h
+++ zone1/MASTER/02-HAL/01-LCD/LCD_private.h
@@ -1,3 +1,19 @@
 #ifndef LCD_PRIVATE_H
 #define LCD_PRIVATE_H
+
+#define LCD_u8_CMD_FUNCTION_SET     0x38
+
+#define LCD_u8_CMD_DISPLAY_ON       0x0C
+
+#define LCD_u8_CMD_CLEAR_DISPLAY    0x01
+
+#define LCD_u8_CMD_ENTRY_MODE       0x06
+
+#define LCD_u8_ROW1_BASE_ADDR       0x80
+#define LCD_u8_ROW2_BASE_ADDR       0xC0
+
+#define LCD_u8_CGRAM_BASE_ADDR      0x40
+
+#define LCD_u8_PATTERN_SIZE         8
+
 #endif
